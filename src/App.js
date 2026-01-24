9e66276a626bf9414b88510538a15dd717d4f32c
Tiffany Fu <tiffany.fu7@gmail.com>
2026-01-23 21:32:30 -0500

    global mask animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
+
+  const containerRef = useRef(null);
+
+  const handleMove = (e) => {
+    const rect = containerRef.current.getBoundingClientRect();
+    const x = (e.clientX - rect.left) / rect.width;
+    containerRef.current.style.setProperty("--reveal", x);
+  };
+
+  const handleTouchMove = (e) => {
+    const rect = containerRef.current.getBoundingClientRect();
+    const x = (e.touches[0].clientX - rect.left) / rect.width;
+    containerRef.current.style.setProperty("--reveal", x);
+  };
+
   return (
     <div>
       <header className="header">
@@ -12,18 +28,23 @@ function App() {
         />
         <h2>mere interference</h2>
       </header>
-      <div className="images">
+      <div
+        ref={containerRef}
+        className="images"
+        onMouseMove={handleMove}
+        onTouchMove={handleTouchMove}
+      >
         <img
-          src="lab.png"
-          alt="Physics Lab Room Image"
-          id="lab"
+          src="rocks.png"
+          alt="Heart Rocks Image"
+          className="img rocks"
           width="800px"
           height="450px"
         />
         <img
-          src="rocks.png"
-          alt="Heart Rocks Image"
-          id="lab"
+          src="lab.png"
+          alt="Physics Lab Room Image"
+          className="img lab"
           width="800px"
           height="450px"
         />
